d2dd01be4c28d2ce182fe0e738f9dc5ef7caa7a9
Trisha Salas <trisha@trishasalas.com>
2015-09-12 13:19:52 -0500

    fix all the scripts that don't want to be minified

---
+++ wp-content/themes/vsc/assets/js/vs-functions.js
@@ -0,0 +1,15 @@
+/*
+ This is where we init all the js/jquery stuff
+ */
+
+jQuery(document).ready(function($) {
+	$( ".description" ).lettering('words');
+	$( ".speaker-name" ).lettering('words');
+
+	$('#responsive-menu').sidr({
+		source: '#mobile-navigation',
+		side: 'right'
+	});
+
+	$("#page-header").sticky({topSpacing:0});
+});
